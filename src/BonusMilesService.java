32b1bd2290fb25bfb8f8759cc00a820c2206fa9b
ustim88 <ustandrei88@gmail.com>
2023-11-18 22:41:41 +0300

    ja

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+        int i = price / 20;
+        return i;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+    }
+}
